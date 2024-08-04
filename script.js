9a4ebdfa14dccc1784c028f860a64aeb6c1eccbd
João Pedro <joaopedro.oliveirapaula@gmail.com>
2024-08-04 18:18:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+//Armazenagem dos dígitos
+const digitos = [... document.querySelectorAll(".dig")];
+const digitosOperacionais = [... document.querySelectorAll(".operator")]
+const digitosFuncionais = [... document.querySelectorAll(".fun")]
+
+//Display
+const display = document.querySelector(".display");
+
+//Armazenagem dos valores digitados
+let valores = [];
+
+// Implementando lógica nos digítos tradicionais
+digitos.map(el=>{
+    el.addEventListener("click",function(evt){
+        
+        if (evt.target.textContent === '.' && valores[valores.length - 1] === '.') {
+            return
+        }
+        valores.push(evt.target.textContent);
+        display.textContent = valores.join("")
+        
+    });
+});
+
+// Implementando lógica nos digítos funcionais
+digitosFuncionais.map(el=>{
+    el.addEventListener("click",(evt)=>{
+        if (evt.target.textContent =='CE'){
+            display.textContent='';
+            valores = [];
+        }else{
+            valores.pop();
+            display.textContent = valores.join("");
+        }
+    });
+});
+
+// Operações
+digitosOperacionais.map((el) => {
+    el.addEventListener("click", function(evt) {
+        // Se o ultimo valor do array for sinal e o usuario clicou em outro, substitui
+        if (['*','-','+','/'].includes(valores[valores.length -1])){
+            valores[valores.length -1] = evt.target.textContent;
+        
+        // se . for o ultimo valor do array retorna
+        }else if(valores[valores.length -1] =='.'){
+            return
+        
+        // Outro caso adiciona
+        }else{
+            valores.push(evt.target.textContent);
+        }
+
+        if (evt.target.textContent=='='){
+            valores = [eval(display.textContent)]
+        }
+        display.textContent = valores.join("");
+    });
+});
